b2793a66ed14ec449c672759a111257ea08a7e65
Jaman-M <jaman.manik1@gmail.comm>
2022-10-30 00:51:14 +0600

    case minus

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+// case plus
 document.getElementById('case-plus').addEventListener('click',function(){
     // console.log('plus clicked');
     const caseInput = document.getElementById('case-number');
     const caseNumber = caseInput.value;
     caseInput.value = parseInt(caseNumber) + 1;
+});
+
+// case minus
+document.getElementById('case-minus').addEventListener('click', function(){
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value = parseInt(caseNumber) - 1;
 })
